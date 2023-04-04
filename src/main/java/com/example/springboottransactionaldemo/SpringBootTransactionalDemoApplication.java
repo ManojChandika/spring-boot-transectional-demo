8c35173f0267a2024e8ce398f65d71227ab273b6
manoj <manojmpmc@gmail.com>
2023-04-04 08:38:56 +0530

    Project Initializer

---
+++ src/main/java/com/example/springboottransactionaldemo/SpringBootTransactionalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottransactionaldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTransactionalDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTransactionalDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboottransactionaldemo/SpringBootTransactionalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottransactionaldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTransactionalDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
